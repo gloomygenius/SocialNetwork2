a06e9a73850a7533443a4d6acbc7df9228d10f78
Vasiliy Bobkov <vasily-bobkow@yandex.ru>
2016-11-07 17:41:59 +0300

    UserDaoImpl refactor

--- src/main/java/com/socialnetwork/dao/h2/ProfileDaoImpl.java
+++ src/main/java/com/socialnetwork/dao/h2/ProfileDaoImpl.java
@@ -1,10 +1,46 @@
 package com.socialnetwork.dao.h2;
 
 import com.socialnetwork.dao.ProfileDao;
+import com.socialnetwork.dao.exception.DaoException;
+import com.socialnetwork.models.Profile;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Optional;
 
 /**
  * Created by Vasiliy Bobkov on 07.11.2016.
  */
-public class ProfileDaoImpl {
+public class ProfileDaoImpl implements ProfileDao {
 
+    @Override
+    public Optional<Profile> getByUserId(long id) throws DaoException {
+        return null;
+    }
+
+    @Override
+    public void add(Profile profile) throws DaoException {
+
+    }
+
+    @Override
+    public void update(Profile profile) throws DaoException {
+
+    }
+
+    @Override
+    public void remove(long userId) throws DaoException {
+
+    }
+
+    LocalDate convertDateToLocalDate(Date date) {
+        if (date == null) return null;
+        LocalDate localDate =
+                Instant.ofEpochMilli(date.getTime())
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDate();
+        return localDate;
+    }
 }
--- src/main/java/com/socialnetwork/dao/h2/UserDaoImpl.java
+++ src/main/java/com/socialnetwork/dao/h2/UserDaoImpl.java
@@ -2,16 +2,13 @@ package com.socialnetwork.dao.h2;
 
 import com.socialnetwork.connection_pool.ConnectionPool;
 import com.socialnetwork.connection_pool.ConnectionPoolException;
-import com.socialnetwork.dao.exception.DaoException;
 import com.socialnetwork.dao.UserDao;
+import com.socialnetwork.dao.exception.DaoException;
 import com.socialnetwork.models.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 import java.sql.*;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Optional;
 
 /**
@@ -24,87 +21,25 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public Optional<User> getById(long id) throws DaoException {
-        Optional<User> user = Optional.empty();
-        try (Connection connection = connectionPool.takeConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(
-                     "SELECT id, email, password, first_name, last_name, gender FROM Users WHERE id='" + id + "'"
-             )) {
-            if (resultSet.next()) {
-                user = Optional.of(createUserFromResultSet(resultSet));
-            }
-        } catch (SQLException | ConnectionPoolException e) {
-            log.warn("Error requesting data from the database", e);
-            throw new DaoException();
-        }
-        return user;
-    }
-
-    User createUserFromResultSet(ResultSet resultSet) {
-        User user = null;
-        try {
-            user = new User(
-                    resultSet.getLong("id"),
-                    resultSet.getString("email"),
-                    resultSet.getString("password"),
-                    resultSet.getString("first_name"),
-                    resultSet.getString("last_name"),
-                    resultSet.getInt("gender")
-            );
-        } catch (SQLException e) {
-            log.error("Error of creating user from result set", e);
-        }
-        return user;
-    }
-
-    LocalDate convertDateToLocalDate(Date date) {
-        if (date == null) return null;
-        LocalDate localDate =
-                Instant.ofEpochMilli(date.getTime())
-                        .atZone(ZoneId.systemDefault())
-                        .toLocalDate();
-        return localDate;
+        String sqlReqest = "SELECT id, email, password, first_name, last_name, gender FROM Users WHERE id='" + id + "'";
+        return getByAny(sqlReqest);
     }
 
     @Override
     public Optional<User> getByEmail(String email) throws DaoException {
-        Optional<User> user = Optional.empty();
-        try (Connection connection = connectionPool.takeConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(
-                     "SELECT id, email, password, first_name, last_name,  gender FROM Users WHERE email = '"
-                             .concat(email)
-                             .concat("'")
-             )) {
-            if (resultSet.next()) {
-                user = Optional.of(createUserFromResultSet(resultSet));
-            }
-        } catch (SQLException | ConnectionPoolException e) {
-            log.error("Error requesting data from the database", e);
-            throw new DaoException();
-        }
-        return user;
+        String sqlReqest = "SELECT id, email, password, first_name, last_name,  gender FROM Users WHERE email = '"
+                .concat(email)
+                .concat("'");
+        return getByAny(sqlReqest);
     }
 
     @Override
     public Optional<User> getByNames(String firstName, String lastName) throws DaoException {
-        Optional<User> user = Optional.empty();
-        try (Connection connection = connectionPool.takeConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(
-                     "SELECT id, email, password, first_name, last_name,  gender FROM Users WHERE "
-                             .concat("first_name='"+firstName+"' AND ")
-                             .concat("last_name='"+lastName+"'")
-             )) {
-            if (resultSet.next()) {
-                user = Optional.of(createUserFromResultSet(resultSet));
-            }
-        } catch (SQLException | ConnectionPoolException e) {
-            log.error("Error requesting data from the database", e);
-            throw new DaoException();
-        }
+        String sqlReqest = "SELECT id, email, password, first_name, last_name,  gender FROM Users WHERE "
+                .concat("first_name='" + firstName + "' AND ")
+                .concat("last_name='" + lastName + "'");
+        return getByAny(sqlReqest);
         // TODO: 07.11.2016 оптимизировать SQL запрос
-        return user;
     }
 
     @Override
@@ -164,4 +99,38 @@ public class UserDaoImpl implements UserDao {
             throw new DaoException();
         }
     }
+
+
+    private User createUserFromResultSet(ResultSet resultSet) {
+        User user = null;
+        try {
+            user = new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("email"),
+                    resultSet.getString("password"),
+                    resultSet.getString("first_name"),
+                    resultSet.getString("last_name"),
+                    resultSet.getInt("gender")
+            );
+        } catch (SQLException e) {
+            log.error("Error of creating user from result set", e);
+        }
+        return user;
+    }
+
+    private Optional<User> getByAny(String sqlRequst) throws DaoException {
+        Optional<User> user = Optional.empty();
+        try (Connection connection = connectionPool.takeConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sqlRequst)) {
+            if (resultSet.next()) {
+                user = Optional.of(createUserFromResultSet(resultSet));
+            }
+        } catch (SQLException | ConnectionPoolException e) {
+            log.warn("Error requesting data from the database", e);
+            throw new DaoException();
+        }
+        return user;
+    }
+
 }
